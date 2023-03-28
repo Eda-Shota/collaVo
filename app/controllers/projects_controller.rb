6011baa7c3701f19a250210efcaa634b823da6c8
EC2 Default User <ec2-user@ip-172-31-33-80.ap-northeast-1.compute.internal>
2023-03-28 02:48:41 +0000

    [Fix]ユーザー非ログイン時の挙動修正

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :joined_index, :create, :edit, :update, :destroy]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
   
   def new
@@ -85,7 +85,7 @@ class ProjectsController < ApplicationController
   def get_comments_list
     if admin_signed_in?
       @project_comments = ProjectComment.all 
-    else
+    elsif user_signed_in?
       @project_comments = ProjectComment.where(user_id: current_user.id).or(ProjectComment.where(user_id: @project.user_id)).where(project_id: @project.id)
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :ensure_correct_user, :ensure_guest_user]
   before_action :ensure_correct_user, only: [:edit, :update]
 
