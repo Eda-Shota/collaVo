2090a561809b60af1dbd0067ff3c7843975a3ecd
EC2 Default User <ec2-user@ip-172-31-33-80.ap-northeast-1.compute.internal>
2023-03-28 04:15:33 +0000

    [Fix]検索時の企画一覧の順番修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,13 +4,13 @@ class SearchesController < ApplicationController
     @word = params[:word]
     @range = params[:range]
     @projects = Project.looks(params[:word],params[:range])
-    @projects = @projects.page(params[:page]).per(10)
+    @projects = @projects.order("created_at desc").page(params[:page]).per(10)
     render "/searches/search_lists.html"  
   end
 
   def search_users_project
     @projects = Project.where(user_id: params[:user_id])
-    @projects = @projects.page(params[:page]).per(10)
+    @projects = @projects.order("created_at desc").page(params[:page]).per(10)
     @user = User.find(params[:user_id])
     render "/searches/search_lists.html" 
   end
